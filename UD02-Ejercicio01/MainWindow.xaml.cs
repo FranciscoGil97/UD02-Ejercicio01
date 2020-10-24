296463342f473257f3b81a704929d732ff135d54
Francisco Julian Gil Marin <fj.gil16@iesdoctorbalmis.com>
2020-10-24 13:27:49 +0200

    Agregar archivos de proyecto.

---
+++ UD02-Ejercicio01/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+
+namespace UD02_Ejercicio01
+{
+    public partial class MainWindow : Window
+    {
+        private int numeroGenerado;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private int GenerarNumero() { return new Random().Next(0, 101); }
+
+        private void ComprobarBoton_Click(object sender, RoutedEventArgs e)
+        {
+            int numeroUsuario = int.Parse(numeroUsuarioTextBox.Text);
+
+            if (numeroUsuario == numeroGenerado)
+                mensajeTextBlock.Text = "¡¡¡Has acertado!!!";
+            else
+                mensajeTextBlock.Text = (numeroUsuario < numeroGenerado ? "Te has quedado corto" : "Te has pasado");
+        }
+
+        private void ReiniciarBoton_Click(object sender, RoutedEventArgs e)
+        {
+            numeroGenerado = GenerarNumero();
+            mensajeTextBlock.Text = "";
+            numeroUsuarioTextBox.Text = "";
+        }
+    }
+}
